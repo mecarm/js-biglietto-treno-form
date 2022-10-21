6ed2cf5c42e77ff10b57e6e241d3df709ca4c6e9
mecarm <melecarmine99@gmail.com>
2022-10-21 10:53:46 +0200

    Fix code and logic of generate ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,25 +4,25 @@ function valida(){
     let valueAge = document.getElementById('eta').value;
     let passengerName = document.getElementById('passenger').value;
     
-    let kmPrice = valueKm * price;
+    if (valueKm == '' || valueAge == '' || passengerName == ''){
+        alert('Compila tutti i campi');
+    }else{
+        let kmPrice = valueKm * price; 
+        let discount;
+        if (valueAge == "minorenne") {
+            discount = 20;
+            document.getElementById('preventivo').innerHTML = (kmPrice - ((kmPrice * discount)  / 100)).toFixed(2) + ' €';
+        } else if (valueAge == "over65"){
+            discount = 40;
+            document.getElementById('preventivo').innerHTML = (kmPrice - ((kmPrice * discount)  / 100)).toFixed(2) + ' €';
+        } else {
+            discount = 0
+            document.getElementById('preventivo').innerHTML = kmPrice.toFixed(2) + ' €';
+        }
         
-
-    let discount;
-    if (valueAge == "minorenne") {
-        discount = 20;
-        document.getElementById('preventivo').innerHTML = (kmPrice - ((kmPrice * discount)  / 100)).toFixed(2) + ' €';
-    } else if (valueAge == "over65"){
-        discount = 40;
-        document.getElementById('preventivo').innerHTML = (kmPrice - ((kmPrice * discount)  / 100)).toFixed(2) + ' €';
-    } else {
-        discount = 0
-        document.getElementById('preventivo').innerHTML = kmPrice.toFixed(2) + ' €';
-    }
-    
-    document.getElementById('passegero').innerHTML = passengerName;
-    document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) +1;
-    document.getElementById("codiceCP").innerHTML = Math.floor(Math.random() * 100000) +10000;
-
+        document.getElementById('passegero').innerHTML = passengerName;
+        document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) +1;
+        document.getElementById("codiceCP").innerHTML = Math.floor(Math.random() * 100000) +10000;}      
 }
 
 function resetta(){
